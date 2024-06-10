e96a5ae439d8cee38bcd6f0c5ce59c096e3c982e
konanangel <dang.vu.dinh.2004@gmail.com>
2024-06-10 14:59:50 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function toggleContactInfo() {
+    var contactDetails = document.getElementById('contact-details');
+    contactDetails.classList.toggle('show');
+}
+
+function toggleLeftColumn() {
+    var leftColumn = document.getElementById('left-column');
+    leftColumn.classList.toggle('active');
+}
